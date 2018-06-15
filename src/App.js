7a6b5bd41fe17708f361cd0cb33b04e28eef2aab
Abelard Lindsay! <dutycat@protonmail.com>
2018-06-15 11:04:49 +0300

    added helmet

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 import Header from "./Header";
 import Main from "./Main";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Redirect } from "react-router";
+import { Redirect, withRouter } from "react-router";
+import Helmet from "react-helmet";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <React.Fragment>
+          <Helmet title="Twitter - EveryInteract" />
           <Header />
           <Main />
           <Redirect to="/EveryInteract" />
