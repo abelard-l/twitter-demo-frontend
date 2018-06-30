9a9066c07cb710e8348d116a3fb79515f3d5bacc
Abelard Lindsay! <dutycat@protonmail.com>
2018-06-30 20:43:49 +0300

    feat: add helmet for generic component

--- src/Generic.js
+++ src/Generic.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
-export default ({ match }) => (
-  <div className="container">
-    <div className="row">
-      <div className="col-xs-offset-3 col-xs-6">
-        {match.path}
+export default ({ match }) => {
+  const pageName = match.path[1].toUpperCase() + match.path.slice(2);
+  return (
+    <React.Fragment>
+      <Helmet title={pageName} />
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-offset-3 col-xs-6">
+            {pageName}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    </React.Fragment>
+  );
+};
